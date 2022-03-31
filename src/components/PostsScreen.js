270e3d05d468a7f3854f510e2e52b77264033a43
Animesh-fragment <animesh.hazarika123@gmail.com>
2022-03-31 20:21:47 +0530

    git push https://github.com/Animesh-fragment/clubhub.git

--- src/components/PostsScreen.js
+++ src/components/PostsScreen.js
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import Posts from './Posts';
 export default function PostsScreen() {
   return (
     <div className="postScreen">
-        <div className='postScreenCard'>
-       <img  className='group-icon'src={require("../assets/codingclub.png")}></img> {/*replace with group icon url*/}
+        <Posts/>
+        {/*<div className='postScreenCard'>
+       <img  className='group-icon'src={require("../assets/codingclub.png")}></img> {/*replace with group icon url
        
-        </div>
+  </div>*/}
     </div>
   )
 }
