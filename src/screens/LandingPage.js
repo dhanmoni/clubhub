66cdd5121a43ed0f4b9e8261cbaa4b258423f23b
Sauvik Nath <sauviknath10@gmail.com>
2022-03-31 22:08:45 +0530

    landing page

--- src/screens/LandingPage.js
+++ src/screens/LandingPage.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import Vector from "../assets/LandingHeroVector.png";
+import Vector from "../assets/vector.png";
 import Vector1 from "../assets/LandingHeroBg.png";
 import { Link } from 'react-router-dom';
 
 function LandingPage() {
   const myStyle = {
-    height: '100vh',
-    backgroundImage: `url(${Vector1})`,
-    backgroundSize: 'cover'
+    backgroundColor: 'DodgerBlue'
   };
   return (
       <div className="container" style={myStyle}>
-        <div>
-          <div className="col-lg-6 mt-xl-5 pt-5">
+
+          <div className="landingpage">
             <h1>Welcome to ClubHub</h1>
             <div>
             <button>Sign In</button>
@@ -21,17 +19,14 @@ function LandingPage() {
            
 
             <h3>We bring in all the clubs of your college together</h3>
-            <p className="blockquote py-4">
-              
-            </p>
-            <Link to="/" className="btn btn-primary mr-3">
+            <Link to="/" className="developers">
               Know More about the developers
             </Link>
           </div>
           <div className="col-lg-6">
             <img className="w-100" src={Vector} alt="vector" />
           </div>
-        </div>
+
       </div>
   )
 }
