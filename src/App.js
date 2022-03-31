d1e7b8f1c05f69cd40b73154d315684e5828bff6
Dhanmoni Nath <dhanmoninath989@gmail.com>
2022-03-31 21:15:51 +0530

    private routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { useEffect } from 'react';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import { Provider } from 'react-redux';
 import Modal from './Components/Modal';
 import {store, persistor} from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react'
 import PostModal from './Components/PostModal';
+
+import Landing from './Pages/Landing';
+import HomeScreen from './Pages/HomeScreen';
+import AdminScreen from './Pages/AdminScreen';
+import PrivateRoute from './Components/PrivateRoute';
+import ClubAdminRoute from './Components/ClubAdminRoute';
+import SuperAdminRoute from './Components/SuperAdminRoute';
+import PostScreen from './Pages/PostScreen';
 
 const App = () => {
   
@@ -12,11 +24,31 @@ const App = () => {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
 
-      <div>
-        <p>Hello</p>
-        <Modal/>
-        <PostModal/>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />}></Route>
+          <Route path="home" element={<HomeScreen />}></Route>
+          <Route
+            exact
+            path="admin"
+            element={
+              <SuperAdminRoute>
+                <AdminScreen />
+              </SuperAdminRoute>
+            }
+          />
+          <Route
+            exact
+            path="post"
+            element={
+              <ClubAdminRoute>
+                <PostScreen />
+              </ClubAdminRoute>
+            }
+          />
+
+        </Routes>
+      </BrowserRouter>
       </PersistGate>
     </Provider>
   );
---
+++ src/Pages/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Modal from '../Components/Modal'
+
+function HomeScreen() {
+  return (
+    <div>
+        <Modal/>
+    </div>
+  )
+}
+
+export default HomeScreen
